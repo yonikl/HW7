0181cf184bd6f08084048090c877a97e4ec23911
yehuda pilent <pylent@g.jct.ac.il>
2022-05-10 22:42:16 +0300

    I added the point class from Exercise 2

--- Point.cpp
+++ Point.cpp
@@ -3,3 +3,55 @@
 //
 
 #include "Point.h"
+#include<iostream>
+#include<cmath>
+
+//The labels are in the file .h
+
+Point::Point()
+{
+    x = 0;
+    y = 0;
+}
+
+Point::Point(int px, int py)
+{
+    x = px;
+    y = py;
+}
+
+void Point::setx(int px)
+{
+    x = px;
+}
+
+int Point::getx()
+{
+    return x;
+}
+
+void Point::sety(int py)
+{
+    y = py;
+}
+
+int Point::gety()
+{
+    return y;
+}
+
+float Point::distance(Point P)
+{
+
+    return sqrt(pow(x - P.x, 2) + pow(y - P.y, 2));
+}
+
+bool Point::operator==(Point a)
+{
+    bool flag = false;
+    if (x == a.getx() && y == a.gety())
+    {
+        flag = true;
+    }
+    return flag;
+}
--- Point.h
+++ Point.h
@@ -6,7 +6,24 @@
 #define HW7_POINT_H
 
 
-class Point {
+class Point
+{
+    int x;
+    int y;
+public:
+    Point();//A basic constructor that starts the codes at 0
+    Point(int, int);//A builder who starts the values before what he gets
+
+    //Definition and acceptance of X
+    void setx(int);
+    int getx();
+
+    //Definition and acceptance of Y
+    void sety(int);
+    int gety();
+
+    float distance(Point);//Calculate distance between two points
+    bool operator==(Point);//An operator that calculates inequality between two points
 
 };
 
