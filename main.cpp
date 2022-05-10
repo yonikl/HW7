8993f4b1bdd88b4f4b8653ebb98a1515fc3c5ba3
yehuda pilent <pylent@g.jct.ac.il>
2022-05-10 22:49:13 +0300

    experience

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 int main() {
 
-    cout << "Hello, World!" << endl;
+    for (int i = 0; i < 100000; ++i) {
+        cout << "Hello, World!" << endl;
+    }
+
     return 0;
 }
